11c10477c11a7f073ee0ac98b69fb1d576f5e7a2
Pelle Mårtenson <pelle.martenson@me.com>
2022-06-14 14:36:12 +0200

    Initial Commit

---
+++ Learning app/Learning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_appApp.swift
+//  Learning app
+//
+//  Created by ab86008 on 2022-06-14.
+//
+
+import SwiftUI
+
+@main
+struct Learning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
